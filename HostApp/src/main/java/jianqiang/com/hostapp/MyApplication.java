cf447fe63633f73ab78562be38f3b6a7d723fac9
baobaojianqiang <16230091@qq.com>
2018-08-17 17:33:32 +0800

    support API 28: H mCallback

--- HostApp/src/main/java/jianqiang/com/hostapp/MyApplication.java
+++ HostApp/src/main/java/jianqiang/com/hostapp/MyApplication.java
@@ -25,7 +25,7 @@ public class MyApplication extends Application {
 
         try {
             AMSHookHelper.hookAMN();
-            AMSHookHelper.hookActivityThread();
+            AMSHookHelper.attachContext();
         } catch (Throwable e) {
             e.printStackTrace();
         }
